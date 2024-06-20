854d23827e9508a5f3ca8f39d43d3b9bb8dc9ba8
Mensura <mensipass@gmail.com>
2024-06-20 16:35:12 +0200

    List group component:onClick,onSelectItem,props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import ListGroup from './Components/ListGroup';
+import ListGroup from "./Components/ListGroup";
 
-function App(){
-  return <div><ListGroup/></div>;
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,22 +1,62 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import { MouseEvent } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
-  if (items.length === 0)
+//pass objct with two properties
+//{items:[], heading: string}
+
+//we use interface
+
+interface Props {
+  items: string[];
+  heading: string;
+  //func that takes item of type string ad is void, (item:string)=>void
+  onSelectItem: (item: string) => void;
+}
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  //let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  //let selectedIndex = 0;
+  //hook, state hook, tell react that this component can have data that will change over time
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  //const [name, setName] = useState(-1);
+  // arr[0]; //variable(selectedIndex)
+  // arr[1]; //updater function
+  //items = [];
+  //const message = items.length === 0 ? <p>No items found</p> : null;
+
+  const getMessage = () => {
+    return items.length === 0 ? <p>No items found</p> : null;
+  };
+  /*if (items.length === 0)
     return (
       <>
         <h1>List</h1>
         <p>No items found.</p>
       </>
     );
-
+*/
+  //event handler, click event
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     <Fragment>
-      <h1>List</h1>
+      <h1>{heading}</h1>
+
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </Fragment>
